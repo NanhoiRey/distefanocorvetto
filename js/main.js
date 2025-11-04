69442eb02783378778fb104abf37835d045916d7
NanhoiRey <nanhoirey94@gmail.com>
2025-11-04 22:25:27 +0100

    Cambio 3

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 function initMenu() {
   const toggle = document.getElementById("menu-toggle");
   const nav = document.querySelector(".nav");
+  const body = document.body;
+  const header = document.querySelector(".site-header");
 
   toggle.addEventListener("click", () => {
     toggle.classList.toggle("active");
     nav.classList.toggle("active");
+    body.classList.toggle("no-scroll");
+    header.classList.toggle("menu-open"); // activa el borde inferior
   });
 }
-
 function initScrollHeader() {
   let lastScroll = 0;
   const header = document.querySelector(".site-header");
